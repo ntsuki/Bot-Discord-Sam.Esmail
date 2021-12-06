9a25ea5fc8cad7b04cc761d51f88852b8575079a
ntsuki <68906460+ntsuki@users.noreply.github.com>
2021-12-06 14:46:24 +0100

    Delete robotGif.py

--- code/robotGif.py
+++
@@ -1,5 +0,0 @@
-robotG = ["https://c.tenor.com/b-t5Dg3vjKMAAAAM/mr-robot.gif", "https://thumbs.gfycat.com/BackPitifulCrow-size_restricted.gif",
-          "https://i.pinimg.com/originals/db/db/ba/dbdbbad5798bfc3ff27a499dc5ca2b30.gif", "https://i.pinimg.com/originals/22/78/fb/2278fb02080ac342fe0aa09c70cfea07.gif",
-          "https://c.tenor.com/aMUOx9-8sOwAAAAC/mr-robot-lol.gif", "https://c.tenor.com/OXENiswYFJQAAAAM/mr-robot-peace.gif",
-          "https://c.tenor.com/Ir1kUWmKU-oAAAAM/mr-robot-korku.gif", "https://c.tenor.com/XasU6FPoaK8AAAAC/mr-robot-smoke.gif",
-          "https://giffiles.alphacoders.com/206/206739.gif", "https://i.pinimg.com/originals/f3/e2/41/f3e24149796a7762dc80dba445befc4e.gif"]
